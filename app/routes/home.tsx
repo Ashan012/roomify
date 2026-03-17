ce0fcd756e8b79cf2591d6f739efb61bcf35e619
ashan012 <khan89891918@outlook.com>
2026-03-17 16:05:10 +0500

    Enhance Home component layout with hero section and call-to-action buttons

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import type { Route } from "./+types/home";
 import NavBar from "../../components/NavBar";
+import { ArrowRight } from "lucide-react";
+import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,8 +11,33 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <div className='home'>
-    <NavBar/>
-    <h1 className="text-3xl text-indigo-700 font-extrabold">Home </h1>
-  </div> ;
+  return (
+    <div className="home">
+      <NavBar />
+      <section className="hero">
+        <div className="announce">
+          <div className="dot">
+            <div className="pulse"></div>
+          </div>
+          <p>Introducing Roomify 2.0</p>
+        </div>
+
+        <h1>Build beautiful spaces at the speed of thougth with roomify</h1>
+
+        <p className="subtitle">
+          Roomify is an AI-first design environment that help you visualize
+          render, and ship artitectural project faster than ever
+        </p>
+
+        <div className="actions">
+          <a href="#upload" className="cta">
+            Start Building <ArrowRight className="icon" />
+          </a>
+          <Button variant="outline" size="lg" className="demo">
+            WATCH DEMO
+          </Button>
+        </div>
+      </section>
+    </div>
+  );
 }

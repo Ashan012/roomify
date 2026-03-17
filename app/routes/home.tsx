6a24dfb0664d2019ab23d944d9454ad5f5ce489c
ashan012 <khan89891918@outlook.com>
2026-03-17 17:17:48 +0500

    Add upload section and project showcase to Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,6 @@
 import type { Route } from "./+types/home";
 import NavBar from "../../components/NavBar";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, ArrowUp, Clock, Layers } from "lucide-react";
 import Button from "components/ui/Button";
 
 export function meta({}: Route.MetaArgs) {
@@ -36,6 +36,64 @@ export default function Home() {
           <Button variant="outline" size="lg" className="demo">
             WATCH DEMO
           </Button>
+        </div>
+
+        <div id="upload" className="upload-shell">
+          <div className="grid-overlay" />
+          <div className="upload-card">
+            <div className="upload-head">
+              <div className="upload-icon">
+                <Layers className="icon" />
+              </div>
+
+              <h3>Upload your floor plan</h3>
+              <p>Supports JPG,PNG formats up to 10MB </p>
+            </div>
+
+            <p>upload images</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="projects">
+        <div className="section-inner">
+          <div className="section-head">
+            <div className="copy">
+              <h2>Projects</h2>
+              <p>
+                Your latest work and shared community project, all in one places
+              </p>
+            </div>
+          </div>
+
+          <div className="projects-grid">
+            <div className="project-card group">
+              <div className="preview">
+                <img
+                  src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+                  alt="projects"
+                />
+
+                <div className="badge">
+                  <span>Community</span>
+                </div>
+              </div>
+              <div className="card-body">
+                <div>
+                  <h3>Project Manhattan</h3>
+
+                  <div className="meta">
+                    <Clock size={12} />
+                    <span>{new Date("01.01.2027").toLocaleDateString()}</span>
+                    <span>By Ashan Jameel</span>
+                  </div>
+                </div>
+                <div className="arrow">
+                  <ArrowUp size={18} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
